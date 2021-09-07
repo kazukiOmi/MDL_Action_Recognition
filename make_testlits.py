9fcab913bcb1c8eef84e8e3f55d5533031e4e38a
Kazuki Omi <k.omi.646@stn.nitech.ac.jp>
2021-09-07 17:11:33 +0900

    make_testlist.pyではなく pandasを使ってmake_testlist.ipynb

--- make_testlits.py
+++ make_testlits.py
@@ -1,7 +1,8 @@
 import csv
+import os
 
 
-def main():
+def get_id():
     filename = '/mnt/NAS-TVS872XT/dataset/Kinetics/kinetics400/test.csv'
     with open(filename, encoding='utf8', newline='') as f:
         csvreader = csv.reader(f)
@@ -11,6 +12,24 @@ def main():
             if num == 1:
                 print(x[num])
                 print("label:{}, id:{}".format(x[num][0], x[num][1]))
+
+
+def get_filename():
+    file_path = "/mnt/NAS-TVS872XT/dataset/Kinetics400/test"
+    files = os.listdir(file_path)
+    print(type(files))
+    print(len(files))
+    print(files[0])
+    result = False
+    for item in files:
+        if "-NKgPEJ_Gsk" in item:
+            result = True
+            return result
+    return False
+
+
+def main():
+    get_filename()
 
 
 if __name__ == '__main__':
