b04a1d9a8d959a6d4f113ba26672aa9f27597d32
omi <k.omi.646@nitech.jp>
2021-11-11 19:34:45 +0900

    学習がまわる．

--- feature_extract/multi_model/test_video_adapter/video_adapter.py
+++ feature_extract/multi_model/test_video_adapter/video_adapter.py
@@ -39,7 +39,7 @@ from torchvision.transforms import (
 import torchinfo
 
 import numpy as np
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 import itertools
 import os
 import pickle
@@ -262,7 +262,7 @@ class ReconstructNetInAdapter(nn.Module):
             model.blocks[3],
         )
 
-        self.adapter0 = select_adapter(adapter_mode, 96, self.num_frame)
+        # self.adapter0 = select_adapter(adapter_mode, 96, self.num_frame)
 
         self.blocks4 = model.blocks[4]
 
@@ -299,7 +299,7 @@ class ReconstructNetInAdapter(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # x = x.permute(0,2,1,3,4)
         x = self.net_bottom(x)
-        x = self.adapter0(x)
+        # x = self.adapter0(x)
         x = self.blocks4(x)
         x = self.adapter1(x)
         x = self.net_top(x)
@@ -309,10 +309,354 @@ class ReconstructNetInAdapter(nn.Module):
         return x
 
 
+def get_kinetics(subset, args):
+    """
+    Kinetics400のデータセットを取得
+
+    Args:
+        subset (str): "train" or "val" or "test"
+
+    Returns:
+        pytorchvideo.data.labeled_video_dataset.LabeledVideoDataset: 取得したデータセット
+    """
+
+    train_transform = Compose([
+        ApplyTransformToKey(
+            key="video",
+            transform=Compose([
+                UniformTemporalSubsample(args.num_frame),
+                transforms.Lambda(lambda x: x / 255.),
+                Normalize((0.45, 0.45, 0.45), (0.225, 0.225, 0.225)),
+                RandomShortSideScale(min_size=256, max_size=320,),
+                RandomCrop(224),
+                RandomHorizontalFlip(),
+            ]),
+        ),
+        RemoveKey("audio"),
+    ])
+
+    val_transform = Compose([
+        ApplyTransformToKey(
+            key="video",
+            transform=Compose([
+                UniformTemporalSubsample(args.num_frame),
+                transforms.Lambda(lambda x: x / 255.),
+                Normalize((0.45, 0.45, 0.45), (0.225, 0.225, 0.225)),
+                ShortSideScale(256),
+                CenterCrop(224),
+            ]),
+        ),
+        RemoveKey("audio"),
+    ])
+
+    transform = val_transform if subset == "val" else train_transform
+
+    root_kinetics = '/mnt/dataset/Kinetics400/'
+
+    if subset == "test":
+        dataset = Kinetics(
+            data_path=root_kinetics + "test_list.txt",
+            video_path_prefix=root_kinetics + 'test/',
+            clip_sampler=RandomClipSampler(
+                clip_duration=64 / 30),
+            video_sampler=RandomSampler,
+            decode_audio=False,
+            transform=transform,
+        )
+        return dataset
+    else:
+        dataset = Kinetics(
+            data_path=root_kinetics + subset,
+            video_path_prefix=root_kinetics + subset,
+            clip_sampler=RandomClipSampler(
+                clip_duration=64 / 30),
+            video_sampler=RandomSampler,
+            decode_audio=False,
+            transform=transform,
+        )
+        return dataset
+
+    return False
+
+
+def get_ucf101(subset, args):
+    """
+    ucf101のデータセットを取得
+
+    Args:
+        subset (str): "train" or "test"
+
+    Returns:
+        pytorchvideo.data.labeled_video_dataset.LabeledVideoDataset: 取得したデータセット
+    """
+    subset_root_Ucf101 = 'ucfTrainTestlist/trainlist01.txt' if subset == "train" else 'ucfTrainTestlist/testlist.txt'
+
+    train_transform = Compose([
+        ApplyTransformToKey(
+            key="video",
+            transform=Compose([
+                UniformTemporalSubsample(args.num_frame),
+                transforms.Lambda(lambda x: x / 255.),
+                Normalize((0.45, 0.45, 0.45), (0.225, 0.225, 0.225)),
+                RandomShortSideScale(min_size=256, max_size=320,),
+                RandomCrop(224),
+                RandomHorizontalFlip(),
+            ]),
+        ),
+        ApplyTransformToKey(
+            key="label",
+            transform=transforms.Lambda(lambda x: x - 1),
+        ),
+        RemoveKey("audio"),
+    ])
+
+    val_transform = Compose([
+        ApplyTransformToKey(
+            key="video",
+            transform=Compose([
+                UniformTemporalSubsample(args.num_frame),
+                transforms.Lambda(lambda x: x / 255.),
+                Normalize((0.45, 0.45, 0.45), (0.225, 0.225, 0.225)),
+                ShortSideScale(256),
+                CenterCrop(224),
+            ]),
+        ),
+        ApplyTransformToKey(
+            key="label",
+            transform=transforms.Lambda(lambda x: x - 1),
+        ),
+        RemoveKey("audio"),
+    ])
+
+    transform = train_transform if subset == "train" else val_transform
+
+    root_ucf101 = '/mnt/dataset/UCF101/'
+    # root_ucf101 = '/mnt/NAS-TVS872XT/dataset/UCF101/'
+
+    dataset = Ucf101(
+        data_path=root_ucf101 + subset_root_Ucf101,
+        video_path_prefix=root_ucf101 + 'video/',
+        clip_sampler=RandomClipSampler(
+            clip_duration=16 / 25),
+        video_sampler=RandomSampler,
+        decode_audio=False,
+        transform=transform,
+    )
+
+    return dataset
+
+
+class LimitDataset(torch.utils.data.Dataset):
+    def __init__(self, dataset):
+        super().__init__()
+        self.dataset = dataset
+        self.dataset_iter = itertools.chain.from_iterable(
+            itertools.repeat(iter(dataset), 2)
+        )
+
+    def __getitem__(self, index):
+        return next(self.dataset_iter)
+
+    def __len__(self):
+        return self.dataset.num_videos
+
+
+def make_loader(dataset, args):
+    """
+    データローダーを作成
+
+    Args:
+        dataset (pytorchvideo.data.labeled_video_dataset.LabeledVideoDataset): get_datasetメソッドで取得したdataset
+
+    Returns:
+        torch.utils.data.DataLoader: 取得したデータローダー
+    """
+    loader = DataLoader(LimitDataset(dataset),
+                        batch_size=args.batch_size,
+                        drop_last=True,
+                        num_workers=args.num_workers,
+                        shuffle=True)
+    return loader
+
+
+class AverageMeter(object):
+    """
+    Computes and stores the average and current value
+    Imported from https://github.com/pytorch/examples/blob/master/imagenet/main.py#L247-L262
+    https://github.com/machine-perception-robotics-group/attention_branch_network/blob/ced1d97303792ac6d56442571d71bb0572b3efd8/utils/misc.py#L59
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        if isinstance(val, torch.Tensor):
+            val = val.item()
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def top1(outputs, targets):
+    batch_size = outputs.size(0)
+    _, predicted = outputs.max(1)
+    return predicted.eq(targets).sum().item() / batch_size
+
+
+def train_adapter(args):
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+
+    train_dataset = get_ucf101("train", args)
+    val_dataset = get_ucf101("val", args)
+    train_loader = make_loader(train_dataset, args)
+    val_loader = make_loader(val_dataset, args)
+
+    model = ReconstructNetInAdapter(args.adapter_mode)
+    model = model.to(device)
+    # model = torch.nn.DataParallel(model)
+    torch.backends.cudnn.benchmark = True
+
+    lr = args.learning_rate
+    weight_decay = args.weight_decay
+    # optimizer = torch.optim.SGD(
+    #     model.parameters(),
+    #     lr=lr,
+    #     momentum=0.9,
+    #     weight_decay=5e-4)
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=lr,
+        betas=(0.9, 0.999),
+        weight_decay=weight_decay)
+    criterion = nn.CrossEntropyLoss()
+
+    hyper_params = {
+        "Dataset": "UCF101",
+        "epoch": args.epoch,
+        "batch_size": args.batch_size,
+        "num_frame": args.num_frame,
+        "optimizer": "Adam(0.9, 0.999)",
+        "learning late": lr,
+        "weight decay": weight_decay,
+        "mode": "train temporal adapter",
+        "Adapter": "adp:0, adp:1",
+    }
+
+    experiment = Experiment(
+        api_key="TawRAwNJiQjPaSMvBAwk4L4pF",
+        project_name="feeature-extract",
+        workspace="kazukiomi",
+    )
+
+    experiment.add_tag('pytorch')
+    experiment.log_parameters(hyper_params)
+
+    step = 0
+    best_acc = 0
+
+    with tqdm(range(args.epoch)) as pbar_epoch:
+        for epoch in pbar_epoch:
+            pbar_epoch.set_description("[Epoch %d]" % (epoch))
+
+            """Training mode"""
+
+            train_loss = AverageMeter()
+            train_acc = AverageMeter()
+
+            with tqdm(enumerate(train_loader),
+                      total=len(train_loader),
+                      leave=True) as pbar_train_batch:
+
+                model.train()
+
+                for batch_idx, batch in pbar_train_batch:
+                    pbar_train_batch.set_description(
+                        "[Epoch :{}]".format(epoch))
+
+                    inputs = batch['video'].to(device)
+                    labels = batch['label'].to(device)
+
+                    bs = inputs.size(0)
+
+                    optimizer.zero_grad()
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
+                    loss.backward()
+                    optimizer.step()
+
+                    train_loss.update(loss, bs)
+                    train_acc.update(top1(outputs, labels), bs)
+
+                    pbar_train_batch.set_postfix_str(
+                        ' | loss_avg={:6.04f} , top1_avg={:6.04f}'
+                        ' | batch_loss={:6.04f} , batch_top1={:6.04f}'
+                        ''.format(
+                            train_loss.avg, train_acc.avg,
+                            train_loss.val, train_acc.val,
+                        ))
+
+                    experiment.log_metric(
+                        "batch_accuracy", train_acc.val, step=step)
+                    experiment.log_metric(
+                        "batch_loss", train_loss.val, step=step)
+                    step += 1
+
+            """Val mode"""
+            model.eval()
+            val_loss = AverageMeter()
+            val_acc = AverageMeter()
+
+            with torch.no_grad():
+                for batch_idx, val_batch in enumerate(val_loader):
+                    inputs = val_batch['video'].to(device)
+                    labels = val_batch['label'].to(device)
+
+                    bs = inputs.size(0)
+
+                    val_outputs = model(inputs)
+                    loss = criterion(val_outputs, labels)
+
+                    val_loss.update(loss, bs)
+                    val_acc.update(top1(val_outputs, labels), bs)
+            """Finish Val mode"""
+
+            pbar_epoch.set_postfix_str(
+                ' train_loss={:6.04f} , val_loss={:6.04f}, train_acc={:6.04f}, val_acc={:6.04f}'
+                ''.format(
+                    train_loss.avg,
+                    val_loss.avg,
+                    train_acc.avg,
+                    val_acc.avg)
+            )
+
+            experiment.log_metric("train_accuracy",
+                                  train_acc.avg,
+                                  step=epoch + 1)
+            experiment.log_metric("train_loss",
+                                  train_loss.avg,
+                                  step=epoch + 1)
+            experiment.log_metric("val_accuracy",
+                                  val_acc.avg,
+                                  step=epoch + 1)
+            experiment.log_metric("val_loss",
+                                  val_loss.avg,
+                                  step=epoch + 1)
+
+    experiment.end()
+
+
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--epoch", type=int, default=5,)
+    parser.add_argument("--epoch", type=int, default=10,)
     parser.add_argument("--batch_size", type=int, default=32,)
+    parser.add_argument("--num_frame", type=int, default=16)
     parser.add_argument("--num_workers", type=int, default=32,)
     parser.add_argument("--learning_rate", type=float, default=1e-3)
     parser.add_argument("--weight_decay", type=float, default=5e-4)
@@ -327,15 +671,7 @@ def get_arguments():
 def main():
     config = configparser.ConfigParser()
     args = get_arguments()
-    model = ReconstructNetInAdapter(args.adapter_mode)
-    torchinfo.summary(
-        model,
-        input_size=(1, 3, 16, 224, 224),
-        depth=2,
-        col_names=["input_size",
-                   "output_size"],
-        row_settings=("var_names",)
-    )
+    train_adapter(args)
 
 
 if __name__ == '__main__':
